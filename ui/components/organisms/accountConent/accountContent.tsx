199829355a5272faee1b155f228095e82a379d90
Vitali Karpuk <vitalikkarpuk652@gmail.com>
2024-02-12 21:24:56 +0300

    fix

--- ui/components/organisms/accountConent/accountContent.tsx
+++ ui/components/organisms/accountConent/accountContent.tsx
@@ -2,47 +2,11 @@ import { ORDER_BY, SORT_BY } from "../../../comman/types";
 import { useTable } from "../../../hooks/useTable";
 import { ConnectWalletButton } from "../../molecules/connectWalletButton";
 import { Table } from "../table";
-import { ScoringConfig } from "./constants";
-import iconMock from "./img/iconMock.svg";
+import { ScoringConfig, mockData } from "./constants";
 import style from "./index.module.css";
 
 const AccountContent = () => {
-  const mockData = {
-    data: [
-      { id: "Evgeniy-dev.mina", url: iconMock, time: 1707757181674 },
-      { id: "mina.mina", url: iconMock, time: 1707757181674 },
-      { id: "Evgeniy-dev.mina", url: iconMock, time: 1707757181674 },
-      { id: "mina.mina", url: iconMock, time: 1707757181674 },
-      { id: "Evgeniy-dev.mina", url: iconMock, time: 1707757181674 },
-      { id: "Vitality.mina", url: iconMock, time: 1707757181674 },
-      { id: "@@@@@@@.mina", url: iconMock, time: 1707757181674 },
-    ],
-    size: 100,
-    totalPages: 1,
-    pageable: {
-      sort: {
-        sorted: true,
-        unsorted: false,
-        empty: false,
-      },
-      offset: 0,
-      pageNumber: 0,
-      pageSize: 100,
-      paged: true,
-      unpaged: false,
-    },
-    last: false,
-    totalElements: 2,
-    number: 0,
-    sort: {
-      sorted: true,
-      unsorted: false,
-      empty: false,
-    },
-    first: true,
-    numberOfElements: 100,
-    empty: false,
-  };
+
   return (
     <div className={style.wrapper}>
       <div className={style.header}>
